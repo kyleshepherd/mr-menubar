228bc95bf2563e957447410817af4eee4d3ae3a4
Kyle Shepherd <kyle@thisissoon.com>
2023-08-29 17:27:57 +0100

    fix(Gitlab): Fix error strings

--- internal/gitlab/gitlab.go
+++ internal/gitlab/gitlab.go
@@ -46,36 +46,36 @@ func GetMRs() (*MRs, error) {
 
 	jsonQuery, err := json.Marshal(queryMap)
 	if err != nil {
-		return nil, fmt.Errorf("Error marshalling JSON instance %v", err)
+		return nil, fmt.Errorf("error marshalling JSON instance %v", err)
 	}
 
 	req, err := http.NewRequest(http.MethodPost, "https://gitlab.com/api/graphql", bytes.NewBuffer(jsonQuery))
 	if err != nil {
-		return nil, fmt.Errorf("Error creating request %v", err)
+		return nil, fmt.Errorf("error creating request %v", err)
 	}
 	req.Header.Set("Content-Type", "application/json")
 	gitToken, err := env.GetEnvVar("GITLAB_TOKEN")
 	if err != nil {
-		return nil, fmt.Errorf("Error getting Gitlab Token %v", err)
+		return nil, fmt.Errorf("error getting Gitlab Token %v", err)
 	}
 	req.Header.Set("Authorization", "Bearer "+gitToken)
 
 	client := &http.Client{Timeout: time.Second * 5}
 	res, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("Error executing request %v", err)
+		return nil, fmt.Errorf("error executing request %v", err)
 	}
 
 	data, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Error reading data %v", err)
+		return nil, fmt.Errorf("error reading data %v", err)
 	}
 
 	resStruct := Response{}
 
 	err = json.Unmarshal(data, &resStruct)
 	if err != nil {
-		return nil, fmt.Errorf("Error unmarshalling JSON %v", err)
+		return nil, fmt.Errorf("error unmarshalling JSON %v", err)
 	}
 
 	return &resStruct.Data.MRs, nil
